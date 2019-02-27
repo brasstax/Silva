2bc90f35bc24cb6676be343e3298ebbdd693f322
Brass Tax <brasstax@users.noreply.github.com>
2019-02-26 23:23:04 -0800

    Initial commit.

---
+++ bot.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+from configparser import ConfigParser
+import logging
+from app.commands import ffrk
+from discord.ext import commands
+import discord
+
+logging.basicConfig(level=logging.INFO)
+
+config = ConfigParser()
+config.read('config.ini')
+
+with open(config['default']['discord_token']) as fp:
+    TOKEN = fp.read()
+COMMAND_PREFIX = config['default']['command_prefix']
+
+bot = commands.Bot(
+    command_prefix=COMMAND_PREFIX,
+    description="The best sniper in Phantagarde...or possibly all the gardes.")
+
+
+
+@bot.event
+async def on_ready():
+    logging.info(f'Logged in as {bot.user.name} ({bot.user.id})')
+    logging.info('------')
+    activity = discord.Game(name=f'{COMMAND_PREFIX}help for help')
+    await bot.change_presence(status=discord.Status.online, activity=activity)
+
+bot.run(TOKEN)
