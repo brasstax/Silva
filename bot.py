33c680c02c5dfb4d8901482026b8c9d3f2f4a0f1
brasstax <brasstax@users.noreply.github.com>
2019-03-02 14:17:06 -0800

    Devel (#1)
    * Added command to retrieve a cat fact and read from an alias database.

--- bot.py
+++ bot.py
@@ -1,9 +1,10 @@
 #!/usr/bin/env python
 from configparser import ConfigParser
 import logging
-from app.commands import ffrk
 from discord.ext import commands
+from silva import bot_commands
 import discord
+import sqlite3
 
 logging.basicConfig(level=logging.INFO)
 
@@ -11,13 +12,33 @@ config = ConfigParser()
 config.read('config.ini')
 
 with open(config['default']['discord_token']) as fp:
-    TOKEN = fp.read()
+    TOKEN = fp.read().strip()
 COMMAND_PREFIX = config['default']['command_prefix']
 
 bot = commands.Bot(
     command_prefix=COMMAND_PREFIX,
     description="The best sniper in Phantagarde...or possibly all the gardes.")
 
+bot.add_cog(bot_commands.SilvaCmds(bot))
+
+
+@bot.event
+async def on_connect():
+    bot.conn = sqlite3.connect('aliases.sqlite3')
+    bot.conn.row_factory = sqlite3.Row
+    bot.cursor = bot.conn.cursor()
+    cmd: str = (
+        "CREATE TABLE IF NOT EXISTS aliases"
+        " (id integer primary key autoincrement,"
+        " word text, alias text, is_proper_noun int DEFAULT 1)"
+    )
+    bot.cursor.execute(cmd)
+    cmd: str = (
+        "CREATE UNIQUE INDEX IF NOT EXISTS"
+        " idx_positions_id ON aliases(id)"
+    )
+    bot.cursor.execute(cmd)
+    bot.conn.commit()
 
 
 @bot.event
