fb221442212f7f9db4d43a9f76abfb031eaa4d08
brasstax <brasstax@users.noreply.github.com>
2019-03-03 00:13:07 -0800

    Updated formatting for bot events (#3)
    Updated formatting for event titles.

--- silva/bot_commands.py
+++ silva/bot_commands.py
@@ -58,7 +58,7 @@ class SilvaCmds(commands.Cog, name="Silva commands"):
         msg = '__CURRENT EVENTS__\n'
         for event in current_events:
             event_text = (
-                f"`{event['title']}` ({event['url']})\n"
+                f"_{event['title']}_ ({event['url']})\n"
                 f"ends {event['finish']}\n"
                 "\n"
             )
@@ -66,7 +66,7 @@ class SilvaCmds(commands.Cog, name="Silva commands"):
         msg += '__FUTURE EVENTS__\n'
         for event in future_events:
             event_text = (
-                f"`{event['title']}` ({event['url']})\n"
+                f"_{event['title']}_ ({event['url']})\n"
                 f"{event['duration']}\n"
             )
             msg += str(event_text)
