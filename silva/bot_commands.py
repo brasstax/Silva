4746c6a84756ffbe8d40473d72db84788288cde3
Brass Tax <brasstax@users.noreply.github.com>
2019-03-02 22:38:24 -0800

    Added events command in silva bot commands.

--- silva/bot_commands.py
+++ silva/bot_commands.py
@@ -3,6 +3,7 @@ import logging
 from discord.ext import commands
 import requests
 import random
+from silva.utilities import gbfwiki
 
 
 class SilvaCmds(commands.Cog, name="Silva commands"):
@@ -12,6 +13,9 @@ class SilvaCmds(commands.Cog, name="Silva commands"):
 
     @commands.command()
     async def song(self, ctx):
+        '''
+        Gets a fact about Song. Or Tweyen. Depends on Silva's mood.
+        '''
         bot = self.bot
         try:
             fact = requests.get(
@@ -40,3 +44,29 @@ class SilvaCmds(commands.Cog, name="Silva commands"):
         except Exception as e:
             logging.warning(f'Could not get cat fact: {e}')
             await ctx.send("I couldn't get a fact at this time, sorry!")
+
+    @commands.command()
+    async def events(self, ctx):
+        '''
+        Gets information from gbf.wiki on current and future events.
+        '''
+        logging.info(f'events requested by {ctx.author}')
+        wiki = gbfwiki.wiki()
+        current_events = wiki.get_current_events()
+        future_events = wiki.get_upcoming_events()
+        msg = '__CURRENT EVENTS__\n'
+        for event in current_events:
+            event_text = (
+                f"`{event['title']}` ({event['url']})\n"
+                f"ends {event['finish']}\n"
+                "\n"
+            )
+            msg += str(event_text)
+        msg += '__FUTURE EVENTS__\n'
+        for event in future_events:
+            event_text = (
+                f"`{event['title']}` ({event['url']})\n"
+                f"{event['duration']}\n"
+            )
+            msg += str(event_text)
+        await ctx.send(msg)
