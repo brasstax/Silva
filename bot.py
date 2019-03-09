20bc8d0a3331b0fc48f2a5be9e9c70b1cfc9f6d5
Brass Tax <brasstax@users.noreply.github.com>
2019-03-09 00:54:44 -0800

    Added streaming tweet functionality to automatically message the #events channel with URLs to @Granblue_en's tweets as they post them.

--- bot.py
+++ bot.py
@@ -3,14 +3,23 @@ from configparser import ConfigParser
 import logging
 from discord.ext import commands
 from silva import bot_commands
+from silva.utilities import granblue_twitter
 import discord
 import sqlite3
 import asyncio
+import argparse
 
-logging.basicConfig(level=logging.INFO)
+loop = asyncio.get_event_loop()
+parser = argparse.ArgumentParser(description='A Granblue Fantasy discord bot.')
+parser.add_argument('--config', '-c', type=str, default='config.ini')
+args = parser.parse_args()
+config_file = args.config
+
+log_format = "%(asctime)s - %(levelname)s - %(message)s"
+logging.basicConfig(level=logging.INFO, format=log_format)
 
 config = ConfigParser()
-config.read('config.ini')
+config.read(config_file)
 
 with open(config['default']['discord_token']) as fp:
     TOKEN = fp.read().strip()
@@ -22,6 +31,9 @@ bot = commands.Bot(
     case_insensitive=True)
 
 bot.add_cog(bot_commands.SilvaCmds(bot))
+
+twitter_config = ConfigParser()
+twitter_config.read(config['default']['twitter_tokens'])
 
 
 @bot.event
@@ -41,6 +53,7 @@ async def on_connect():
     )
     bot.cursor.execute(cmd)
     bot.conn.commit()
+    setattr(bot, 'twitter_stream', None)
 
 
 @bot.event
@@ -49,13 +62,22 @@ async def on_ready():
     logging.info('------')
     activity = discord.Game(name=f'{COMMAND_PREFIX}help for help')
     await bot.change_presence(status=discord.Status.online, activity=activity)
+    twitter = granblue_twitter.Twitter(
+        bot=bot,
+        consumer=twitter_config['default']['api'],
+        consumer_secret=twitter_config['default']['api_secret'],
+        access=twitter_config['default']['access_token'],
+        access_secret=twitter_config['default']['access_token_secret'],
+        discord_channel_id=config['twitter']['discord_channel_id'],
+        twitter_user_id=config['twitter']['twitter_user_id'])
+    await twitter.follow()
 
-loop = asyncio.get_event_loop()
+
 try:
     loop.run_until_complete(bot.login(token=TOKEN))
     loop.run_until_complete(bot.connect())
 except KeyboardInterrupt:
-    logging.info('Logging out.')
+    logging.info('Logging out. (You might need to ctrl-C twice.)')
     loop.run_until_complete(bot.logout())
 finally:
     loop.close()
--- silva/utilities/granblue_twitter.py
+++ silva/utilities/granblue_twitter.py
@@ -3,116 +3,46 @@
 # A collection of utilities to get tweet information from
 # @Granblue_en.
 
-import tweepy
-from discord.ext import commands
+import peony
 import logging
-from tweepy import Status
-import json
-import re
-from tweepy.error import TweepError
-from tweepy.streaming import ReadBuffer
 
 
-class TwitterAuth(object):
+class Twitter(object):
     def __init__(
             self,
+            bot,
             consumer: str,
             consumer_secret: str,
             access: str,
-            access_secret: str):
-        self.auth = tweepy.OAuthHandler(consumer, consumer_secret)
-        self.auth.set_access_token(access, access_secret)
-        self.api = tweepy.API(self.auth)
-
-
-class GranblueStream(tweepy.Stream):
-    async def _data(self, data):
-        if await self.listener.on_data(data) is False:
-            self.running = False
-
-    async def _read_loop(self, resp):
-        charset = resp.headers.get('content-type', default='')
-        enc_search = re.search('charset=(?P<enc>\S*)', charset)
-        if enc_search is not None:
-            encoding = enc_search.group('enc')
-        else:
-            encoding = 'utf-8'
-
-        buf = ReadBuffer(resp.raw, self.chunk_size, encoding=encoding)
-
-        while self.running and not resp.raw.closed:
-            length = 0
-            while not resp.raw.closed:
-                line = buf.read_line()
-                # line is sometimes None so we need to check here
-                stripped_line = line.strip() if line else line
-                if not stripped_line:
-                    # keep-alive new lines are expected
-                    self.listener.keep_alive()
-                elif stripped_line.isdigit():
-                    length = int(stripped_line)
-                    break
-                else:
-                    raise TweepError(
-                        'Expecting length, unexpected value found')
-
-            next_status_obj = buf.read_len(length)
-            if self.running and next_status_obj:
-                await self._data(next_status_obj)
-
-
-class GranblueListener(tweepy.StreamListener):
-    def __init__(self, bot: commands.Bot, channel: int, user: int):
-        super(GranblueListener, self).__init__()
+            access_secret: str,
+            discord_channel_id: int,
+            twitter_user_id: int):
+        self.client = peony.PeonyClient(
+            consumer_key=consumer,
+            consumer_secret=consumer_secret,
+            access_token=access,
+            access_token_secret=access_secret
+        )
         self.bot = bot
-        self.channel_id = channel
-        self.user_id = user
+        self.channel_id = int(discord_channel_id)
+        self.follow_id = int(twitter_user_id)
 
-    async def on_data(self, raw_data):
-        data = json.loads(raw_data)
-        if 'in_reply_to_status_id' in data:
-            status = Status.parse(self.api, data)
-            if await self.on_status(status) is False:
-                return False
-        elif 'delete' in data:
-            delete = data['delete']['status']
-            if self.on_delete(delete['id'], delete['user_id']) is False:
-                return False
-        elif 'event' in data:
-            status = Status.parse(self.api, data)
-            if self.on_event(status) is False:
-                return False
-        elif 'direct_message' in data:
-            status = Status.parse(self.api, data)
-            if self.on_direct_message(status) is False:
-                return False
-        elif 'friends' in data:
-            if self.on_friends(data['friends']) is False:
-                return False
-        elif 'limit' in data:
-            if self.on_limit(data['limit']['track']) is False:
-                return False
-        elif 'disconnect' in data:
-            if self.on_disconnect(data['disconnect']) is False:
-                return False
-        elif 'warning' in data:
-            if self.on_warning(data['warning']) is False:
-                return False
-        else:
-            logging.error("Unknown message type: " + str(raw_data))
-
-    async def on_status(self, status):
-        # Ignore retweets
-        if status.user.id != self.user_id:
-            return
+    async def follow(self):
+        client = self.client
+        await client.user
         bot = self.bot
         channel = bot.get_channel(self.channel_id)
-        screen_name = status.user.screen_name
-        status = status.id
-        msg = f'https://twitter.com/@{screen_name}/status/{status}'
-        await channel.send(msg)
-
-    def on_error(self, status_code):
-        if status_code == 420:
-            logging.warning('Twitter rate-limit in effect.')
-            return True
+        req = client.stream.statuses.filter.post(follow=self.follow_id)
+        async with req as stream:
+            async for tweet in stream:
+                if peony.events.tweet(tweet):
+                    status_id = tweet.id
+                    username = tweet.user.screen_name
+                    user_id = tweet.user.id
+                    if user_id != self.follow_id:
+                        logging.info(f'Ignoring retweet from @{username}')
+                        return
+                    logging.info(f"@{username}: {tweet.text}")
+                    url = f"https://twitter.com/{username}/status/{status_id}"
+                    logging.info(url)
+                    await channel.send(url)
