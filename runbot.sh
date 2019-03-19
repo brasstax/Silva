d9ecd7fa28d1f5d4f86a34a304cad0ebfe56f1f7
Brass Tax <brasstax@users.noreply.github.com>
2019-03-18 22:54:56 -0700

    Added a bash script to restart the bot upon abnormal crash.

---
+++ runbot.sh
@@ -0,0 +1,7 @@
+cd "$(dirname "$0")"
+config=${1:-config.ini}
+echo "using $config for config"
+until /usr/bin/env python3 bot.py --config $config; do
+  echo "Silva crashed with an exit code $?. Respawning..." >&2
+  sleep 1
+done
