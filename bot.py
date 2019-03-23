332a426a2fae95b6352bba1260d80e407785a002
Brass Tax <brasstax@users.noreply.github.com>
2019-03-23 11:51:51 -0700

    Failed messages now silently fail, instead just logging to console.

--- bot.py
+++ bot.py
@@ -87,10 +87,7 @@ async def on_command_error(ctx, *args, **kwargs):
         guild = 'direct message'
     msg = f'{ctx.author} from {guild} caused an error: {warning}'
     logging.warning(f'message: {msg}')
-    mention = ctx.author.mention
-    content = ctx.message.content
-    r = f"{mention}, I don't know what `{content}` means."
-    await ctx.send(r)
+    pass
 
 try:
     loop.run_until_complete(bot.login(token=TOKEN))
