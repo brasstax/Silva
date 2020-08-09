7177e4fd454f00d32eb081b0289cd9975d698db3
brasstax <brasstax@users.noreply.github.com>
2020-08-08 22:14:25 -0700

    runbot.sh now sleeps for 60 seconds instead of 10.

--- runbot.sh
+++ runbot.sh
@@ -3,5 +3,5 @@ config=${1:-config.ini}
 echo "using $config for config"
 until /usr/bin/env python3 bot.py --config $config; do
   echo "Silva crashed with an exit code $?. Respawning..." >&2
-  sleep 10
+  sleep 60
 done
